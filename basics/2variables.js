49338ba03127c6c7a9eae34a3b68efc7daa864ad
Shwetabh vikram <108172880+Vikramshwetabh@users.noreply.github.com>
2025-06-10 10:32:17 +0000

    conversion

---
+++ basics/2variables.js
@@ -0,0 +1,5 @@
+const accountId = 137487
+let accountEmail = "vikramshwetabh2@gmail.com"
+var password = "797ehu"
+
+console.table([accountId, accountEmail, password])//we use table to print multiple values
---
+++ basics/4conversion.js
@@ -0,0 +1,30 @@
+let score = "33"
+console.log(score)
+
+console.log(typeof score)
+
+let name = "Shwetabh"
+let convertoNum = Number(score)
+console.log(convertoNum)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isloggedin = 1
+
+let boolisloggedin = Boolean(isloggedin)
+
+console.log(boolisloggedin)
+
+// 1 => true; 0 => false
+// "" => false
+// "Shwetabh" => true
+
+
+let string = 6
+
+let intToString = String(string)
+
+console.log(intToString)
+console.log(typeof intToString)
